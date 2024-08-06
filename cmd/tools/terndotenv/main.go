a621e75d551ec87dfd864fb63040a007eed6352d
bbcvasconcellos <bbcvasconcellos@gmail.com>
2024-08-06 23:10:57 +0100

    add: tern, tern.config, docker-compose

---
+++ cmd/tools/terndotenv/main.go
@@ -0,0 +1,28 @@
+package main
+
+import (
+	"log"
+	"os/exec"
+
+	"github.com/joho/godotenv"
+)
+
+func main() {
+	err := godotenv.Load()
+	if err != nil {
+		log.Fatal("Error loading .env file")
+	}
+
+	cmd := exec.Command(
+		"tern",
+		"migrate",
+		"--migrations",
+		"./internal/store/pgstore/migrations",
+		"--config",
+		"./internal/store/pgstore/migrations",
+	)
+
+	if err := cmd.Run(); err != nil {
+		panic(err)
+	}
+}
