01ab44e1e2739bf766e6797d4c3fb2e2f18e28f2
bbcvasconcellos <bbcvasconcellos@gmail.com>
2024-08-12 11:09:15 +0100

    add: new packages and dependencias

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -3,6 +3,8 @@ import { Toaster } from "sonner";
 
 import { CreateRoom } from "./pages/create-room";
 import { Room } from "./pages/room";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib/react-query";
 
 function App() {
 
@@ -19,10 +21,10 @@ function App() {
   ])
 
   return (
-    <>
+    <QueryClientProvider client={queryClient}>
       <RouterProvider router={router} />
       <Toaster position="top-right" richColors invert/>
-    </>
+    </QueryClientProvider>
   )
 }
 
